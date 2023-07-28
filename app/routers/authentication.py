87c6c8433054c2ae3d812f1779433326129175c3
Alexandros Tzimas <tzalex@outlook.com.gr>
2023-07-28 10:44:07 +0300

    Add login success page

--- app/routers/authentication.py
+++ app/routers/authentication.py
@@ -2,6 +2,7 @@ from typing import Annotated
 from fastapi.responses import HTMLResponse
 from fastapi import (APIRouter, status, Request,
                      HTTPException, Depends, Form, Response)
+from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 from app.security import authenticate_user, create_access_token
 from app.database import get_db
@@ -26,10 +27,12 @@ async def login_form(request: Request):
 
 
 @router.post("/set_auth_cookie", 
-             status_code=status.HTTP_201_CREATED)
+             status_code=status.HTTP_201_CREATED,
+             response_class=HTMLResponse)
 def login(username: Annotated[str, Form()], 
           password:  Annotated[str, Form()],
           response: Response,
+          request: Request,
           db: Session = Depends(get_db)):
     """
     Creates a token and saves it in a cookie inside the browser. 
@@ -41,10 +44,14 @@ def login(username: Annotated[str, Form()],
     token = Token(access_token=create_access_token(data={'sub': user.username}))
     # On "expires" cookie param bellow I convert the seconds to minutes since
     #  the expires == how many seconds after should it expire.
-    response.set_cookie(key="token", 
-                        value=token.access_token,
-                        expires=int(environ["ACCESS_TOKEN_EXPIRE_MINUTES"])*60)
-    return {"You have successfully logged in!"}
+    template_response = templates.TemplateResponse('login_success.html', 
+                                                   {"request": request})
+    template_response.set_cookie(
+        key="token", 
+        value=token.access_token,
+        expires=int(environ["ACCESS_TOKEN_EXPIRE_MINUTES"])*60)
+    
+    return template_response
 
 
 @router.delete("/logout", status_code=status.HTTP_205_RESET_CONTENT)
